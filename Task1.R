0f654a28a646bf353b74588923f9ca51429d4bfb
Cedric <Cedric>
2016-06-21 11:41:20 +0200

    a ertser Punkt

---
+++ Task1.R
@@ -0,0 +1,52 @@
+library(magrittr)
+library(dplyr)
+
+trainDataAll <- read.csv("basketball_train.csv", sep = ";")
+
+trainData <- head(trainData,16000)
+
+#a
+#
+trainData %>%
+  select(playoffs, shot_made_flag, shot_type)%>%
+  filter(.,playoffs == 0 ) %>%                 
+  filter(.,shot_type == "2PT Field Goal" ) -> Season2Point   
+
+  Season2Point %>%
+   filter(.,shot_made_flag == 0 ) -> Season2PointNM
+        
+(count(Season2PointNM)/count(Season2Point)) #52,46% Season 2-Points Not Made
+
+  trainData %>%
+    select(playoffs, shot_made_flag, shot_type)%>%
+    filter(.,playoffs == 1 ) %>%                 
+    filter(.,shot_type == "2PT Field Goal" ) -> Season2Point   
+  
+  Season2Point %>%
+    filter(.,shot_made_flag == 0 ) -> Season2PointNM
+  
+  (count(Season2PointNM)/count(Season2Point)) #47,54% Playoffs 2-Points Not Made
+  
+trainData %>%
+  select(playoffs, shot_made_flag, shot_type)%>%
+  filter(.,playoffs == 0 ) %>%                  
+  filter(.,shot_type == "3PT Field Goal" ) -> Season3Point
+
+  Season3Point %>%
+    filter(.,shot_made_flag == 0 ) -> Season3PointNM               
+                                              
+  (count(Season3PointNM)/count(Season3Point)) ->  #66,73% Season 3-Points Not Made
+
+  trainData %>%
+    select(playoffs, shot_made_flag, shot_type)%>%
+    filter(.,playoffs == 1 ) %>%                  
+    filter(.,shot_type == "3PT Field Goal" ) -> Season3Point
+  
+  Season3Point %>%
+    filter(.,shot_made_flag == 0 ) -> Season3PointNM               
+  
+  (count(Season3PointNM)/count(Season3Point)) -> #69,57 Playoffs 3-Points Not Made
+    
+
+
+ 
